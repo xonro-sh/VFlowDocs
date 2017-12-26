75cbca5eca03d8a5e5cb9d61c6e306fd7dc6e448
louie-001 <louie@xonro.com>
2017-12-26 10:01:30 +0800

    Initial commit

---
+++ 源代码管理/后端代码/VFlow/src/main/java/com/xonro/vflow/VFlowApplication.java
@@ -0,0 +1,12 @@
+package com.xonro.vflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VFlowApplication.class, args);
+	}
+}
